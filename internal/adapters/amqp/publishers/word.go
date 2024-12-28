fc85653370cfbf3a720f5ede65d51a48a1d86f80
Bruce Neco <bruce.neco@gmail.com>
2024-12-28 20:52:32 -0300

    feat(amqp): add word publisher

---
+++ internal/adapters/amqp/publishers/word.go
@@ -0,0 +1,40 @@
+package publishers
+
+import (
+	"context"
+	"dicio-scrapper/internal/adapters/amqp/settings"
+	"dicio-scrapper/internal/ports/wordports"
+
+	"github.com/rs/zerolog/log"
+	"github.com/wagslane/go-rabbitmq"
+	"go.uber.org/fx"
+)
+
+type Word struct {
+	p *rabbitmq.Publisher
+}
+
+func NewWord(conn *settings.Connection, lc fx.Lifecycle) wordports.Publisher {
+	publisher, err := conn.MakePublisher(settings.ExchangeOpts{
+		Name: settings.DefaultExchangeName,
+		Kind: settings.DefaultExchangeType,
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot initialize extract word amqp publisher")
+	}
+
+	lc.Append(fx.Hook{
+		OnStop: func(_ context.Context) error {
+			publisher.Close()
+			return nil
+		},
+	})
+
+	return &Word{p: publisher}
+}
+
+func (e *Word) ExtractWord(ctx context.Context, word string) error {
+	return e.p.PublishWithContext(ctx, []byte(word), []string{"extract_word"},
+		rabbitmq.WithPublishOptionsPersistentDelivery,
+	)
+}
