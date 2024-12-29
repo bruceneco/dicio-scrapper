da3d52285b42cfe011f53f3429c4b0b009948762
Bruce Neco <bruce.neco@gmail.com>
2024-12-28 23:06:52 -0300

    refactor: bind constructor to interface of word amqp

--- internal/adapters/amqp/module.go
+++ internal/adapters/amqp/module.go
@@ -4,6 +4,7 @@ import (
 	"dicio-scrapper/internal/adapters/amqp/consumers"
 	"dicio-scrapper/internal/adapters/amqp/publishers"
 	"dicio-scrapper/internal/adapters/amqp/settings"
+	"dicio-scrapper/internal/ports/wordports"
 
 	"go.uber.org/fx"
 )
@@ -12,6 +13,9 @@ var Module = fx.Options(
 	fx.Provide(
 		settings.NewConnection,
 		publishers.NewWord,
+		func(p *publishers.Word) wordports.Publisher {
+			return p
+		},
 	),
 	fx.Invoke(
 		consumers.StartExtractWord,
