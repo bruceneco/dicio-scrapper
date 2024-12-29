b2730378709432dbb74c6880a0d9a30d873bfb4e
Bruce Neco <bruce.neco@gmail.com>
2024-12-28 23:04:57 -0300

    feat: make scrape return word on wordports

--- internal/ports/wordports/scrapper.go
+++ internal/ports/wordports/scrapper.go
@@ -1,6 +1,10 @@
 package wordports
 
+import (
+	"dicio-scrapper/internal/domain/core"
+)
+
 type Scrapper interface {
-	Scrape(word string) ([]string, error)
+	Scrape(word string) (core.Word, error)
 	MostSearched(page int) []string
 }
